95a18d6772074073ae1920466a17f26b8b107db8
hi-punith <isthatyoupunith@gmail.com>
2026-06-05 00:14:08 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,7 @@
+print("===================================")
+print("   Jenkins Build Successful 🚀")
+print("===================================")
+print("Hello from GitHub and Jenkins!")
+
+for i in range(1, 6):
+    print(f"Step {i} completed")
