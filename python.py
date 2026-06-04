7730c92f8bab3d9b8c48522025bb73334979914d
hi-punith <isthatyoupunith@gmail.com>
2026-06-05 01:27:41 +0530

    two change python

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
 print("===================================")
 print("   Jenkins Build Successful 🚀")
 print("===================================")
-print("Hello from GitHub and Jenkins!change")
+print("Hello from GitHub and Jenkins!change two")
 
 for i in range(1, 6):
     print(f"Step {i} completed")
